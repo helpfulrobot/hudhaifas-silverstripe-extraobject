811312a351a1ec94af0cb2c4d2f2497d47fe6ff1
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2016-12-22 15:57:32 -0600

    - Added Parent and Home pages.

---
+++ code/pages/ParentPage.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * MIT License
+ *  
+ * Copyright (c) 2016 Hudhaifa Shatnawi
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ *
+ * @author Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
+ * @version 1.0, May 1, 2015 - 2:29:36 PM
+ */
+class ParentPage
+        extends Page {
+
+    private static $icon = 'extraobject/images/icon-folder.png';
+    private static $description = 'Parent page with no content.';
+
+    public function getCMSFields() {
+        $fields = parent::getCMSFields();
+        $fields->removeFieldFromTab("Root.Main", "Content");
+
+        return $fields;
+    }
+
+}
+
+class ParentPage_Controller
+        extends Page_Controller {
+    
+}
